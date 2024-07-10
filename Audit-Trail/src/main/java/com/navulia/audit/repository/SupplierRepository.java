3aa961bff7f9922527dfbf3b6a3a210d590de2b1
Arun Chouhan <108116902+arunchouhan07@users.noreply.github.com>
2024-07-10 17:47:31 +0530

    Delete Audit-Trail directory

--- Audit-Trail/src/main/java/com/navulia/audit/repository/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.navulia.audit.repository;
-
-import com.navulia.audit.entity.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier,Long> {
-}
